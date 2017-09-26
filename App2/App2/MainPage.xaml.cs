0dcd483288a8eb635a047e436b3e1b2c6e24be16
DESKTOP-SO17ACK\ben <ben@buildcalc.com>
2017-09-26 11:59:23 -0400

    Inital Commit

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1(false));
+        }
+
+        private void Button_Clicked2(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Page1(true));
+        }
+
+    }
+}
---
+++ App2/App2/Page1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+    public class Page1 : ContentPage
+    {
+        bool _modal = false;
+        public Page1(bool modal)
+        {
+            _modal = modal;
+
+            var button = new Button
+            {
+                Text = "return to MainPage",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label
+                    {
+                        Text = "Welcome to Xamarin Forms!",
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.Center
+                    },
+                    button
+                }
+            };
+
+            button.Clicked += Button_Clicked;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (_modal)
+                Navigation.PopModalAsync();
+            else
+                Navigation.PopAsync();
+        }
+    }
+}
